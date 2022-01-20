38185d7a09092f5281a20c27eff851c2b54a6df5
Nikita Malinovsky <66699290+nikitamalinov@users.noreply.github.com>
2022-01-19 23:07:11 -0800

    Update template.js

--- template.js
+++ template.js
@@ -2,29 +2,35 @@ function connect() {
   if(window.ethereum) {
     window.ethereum.request({method: 'eth_requestAccounts'})
     .then(result => {
+      
       accountChangedHandler(result[0]);
 
     })
 
   } else {
-    console.log('Install MetaMask');
+    window.open("https://metamask.io/", "_blank")
+    
+   
   }
 }
 
 const accountChangedHandler = (newAccount) => {
-  console.log("new acc: " + newAccount) 
-  
-  try{
-    btn = document.getElementById("connectWallet")
-    btn.innerHTML = newAccount; //display the network too
-    btn.classList.remove("connect-wallet")
-    btn.classList.add("connected-wallet")
-  }  
-  catch(error)
-  {
-
+  let btn = document.getElementById("connectWallet")
+  if (ethereum.chainId != 1){
+    btn.innerHTML = "Please change to Eth Network"
   }
-  
-
-
+  else
+  {
+    btn.innerHTML = "ETH | " + newAccount.substring(0,6) + "..." + newAccount.slice(-4); //display the network too 
+  }
+  btn.classList.remove("connect-wallet")
+  btn.classList.add("connected-wallet")
 }
+
+
+
+
+//make then stay on the USDC chain
+ethereum.on('chainChanged', (_chainId) => window.location.reload());
+
+
